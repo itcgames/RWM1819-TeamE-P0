c2770f4cfd1f8eb669ad2833eee8bc9327e35147
Brandon Seah-Dempsey <C00204076@itcarlow.ie>
2018-09-25 13:30:57 +0100

    first commit

---
+++ src/p0TeamE.js
@@ -0,0 +1,6 @@
+/*! p0TeamE v0.0.0 - MIT license */
+'use strict';
+
+var p0TeamE = function () {
+  // your code goes here
+}
---
+++ test/p0TeamE.js
@@ -0,0 +1,20 @@
+/* global p0TeamE, describe, it, expect, should */
+
+describe('p0TeamE()', function () {
+  'use strict';
+
+  it('exists', function () {
+    expect(p0TeamE).to.be.a('function');
+
+  });
+
+  it('does something', function () {
+    expect(true).to.equal(false);
+  });
+
+  it('does something else', function () {
+    expect(true).to.equal(false);
+  });
+
+  // Add more assertions here
+});
