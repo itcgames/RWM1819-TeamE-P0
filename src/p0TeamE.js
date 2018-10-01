51a1c069647273a479822a40a92563bc04f719ae
David Nolan <davidnln13@yahoo.com>
2018-10-01 16:10:38 +0100

    finished main function

--- src/p0TeamE.js
+++ src/p0TeamE.js
@@ -3,6 +3,41 @@
 
 var p0TeamE = function (x) {
 
-  /*return Cube(x)*/
-  // your code goes here
+  //Return the array cubed
+  var cubeResultArray = [];
+
+  for(var i = 0; i<x.length; i++)
+  {
+    cubeResultArray.push(Cube(x[i]));
+  }
+//  return cubeResultArray;
+
+  //return the array modulo 3
+  var modulo_3ResultArray = [];
+
+  for(var i = 0; i<x.length; i++)
+  {
+    modulo_3ResultArray.push(modulo_3(cubeResultArray[i]));
+  }
+  //return modulo_3ResultArray
+
+  //return the array Modulo_First
+  var Modulo_FirstResultArray = Modulo_First(modulo_3ResultArray);
+  //return Modulo_FirstResultArray;
+
+  //return the array decrement_if_less_than_first
+  var DecrementResultArray = decrement_if_less_than_first(Modulo_FirstResultArray);
+//  return DecrementResultArray;
+
+  //Return the array running_total
+  total = 0;
+  input = 0;
+  var runningTotalResultArray = [];
+
+  for(var i = 0; i<x.length; i++)
+  {
+    runningTotalResultArray.push(running_total(DecrementResultArray[i]));
+  }
+  return runningTotalResultArray;
+
 }
--- test/p0TeamE.js
+++ test/p0TeamE.js
@@ -7,6 +7,29 @@ describe('p0TeamE()', function () {
     expect(p0TeamE).to.be.a('function');
 
   });
+  // it('Returns a cubed array', function () {
+  //   expect(p0TeamE([6,5,4,3,2,1,7,8,9])).to.eql([216,125,64,27,8,1,343,512,729]);
+  //
+  // });
+  // it('cubes an array then returns the result of modulo 3', function () {
+  //   expect(p0TeamE([216,125,64,27,8,1,343,512,729])).to.eql([0,2,1,0,2,1,1,2,0]);
+  //
+  // });
+  // it('cubes an array gets the result of modulo 3 then returns the result Modulo_First', function () {
+  //   expect(p0TeamE([0,2,1,0,2,1,1,2,0])).to.eql([0,2,1,0,2,1,1,2,0]);
+  //
+  // });
+  // it('cubes an array gets the result of modulo 3 then gets the result of Modulo_First then returns the result of decrement_if_less_than_first', function () {
+  //   expect(p0TeamE([0,2,1,0,2,1,1,2,0])).to.eql([0,2,1,0,2,1,1,2,0]);
+  //
+  // });
+  // it('cubes an array gets the result of modulo 3 then gets the result of Modulo_First then gets the result of decrement_if_less_than_first then returns the result of running_total', function () {
+  //   expect(p0TeamE([0,2,1,0,2,1,1,2,0])).to.eql([0,2,3,0,5,6,7,9,0]);
+  //
+  // });
+  it('Returns an array after calculations', function () {
+    expect(p0TeamE([6,5,4,3,2,1,7,8,9])).to.eql([0,2,3,3,5,6,7,9,9]);
 
+  });
   // Add more assertions here
 });
